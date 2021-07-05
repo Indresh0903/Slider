54bd51031d31a66f24fbcf513090e220f0eff138
Indresh0903 <igupta@ce.iitr.ac.in>
2021-07-05 11:48:27 +0530

    css

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -4,14 +4,16 @@ import Footer from './Footer'
 import Select from 'react-dropdown-select'
 import { Options } from './Options';
 export default function Email() {
-    const setValues=()=>{    
+    const setValues=(e)=>{ 
+        console.log(e)
+        
     }
     const options=Options;
     const list=options.map((option,index)=>{
         return(
             <div className="selectors" key={index}>
                 <label>{option.field}</label>
-            <Select className="package" options={option.option} onChange={(values) => setValues(values)}
+            <Select className="package" options={option.option} onChange={(e) => setValues(e)}
              placeholder={"Select"} clearable={true} closeOnSelect={false}
             labelField={'name'} searchBy={"name"} clearOnSelect={false} style={{color:"#4a4848",background:"#ecedeb",textAlign:"center"}} color={"#bfbfbf"}  required={true}
             />
@@ -24,10 +26,10 @@ export default function Email() {
             {/* <MainHeader/> */}
             {/* <div className="page-name">
             </div> */}
-            
+            <form className="form">
             {list}
-            
-
+            <input type="submit" className="email-submit"/>
+            </form>
             <Footer/>
         </div>
     )
